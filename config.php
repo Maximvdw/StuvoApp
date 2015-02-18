3d4770414ea84f579eaafcf979137e221e707098
Maxim Van de Wynckel <maxim@mvdw-software.com>
2015-02-18 16:19:37 -0500

    Phonegap integration

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$config['googleapi']['name'] = 'StuvoApp';
-$config['googleapi']['key'] = 'AIzaSyBZkAZNC9zLIhTbdWT3NxfEXyoOjqKp6R8';
-$config['googleapi']['calendarid'] = 'qj9ec7js3ef356bcfof58u8pv8@group.calendar.google.com';
-?>
