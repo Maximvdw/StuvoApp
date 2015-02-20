177636e23513ad3d211c19f2cba6a3967bb03ed1
Maxim Van de Wynckel <maxim@mvdw-software.com>
2015-02-20 04:23:36 -0500

    wijzigingen gui

--- backend/html/agenda.php
+++ backend/html/agenda.php
@@ -23,13 +23,21 @@ $events = $cal->events->listEvents($config['googleapi']['calendarid'], $params);
 // Output array
 $output = array();
 
+$currentMonth = date('n');
+
 $i = 0;
+$monthStr = "Deze Maand";
 foreach ($events->getItems() as $event) {
 	$eventName = $event->summary;
 	$eventDateStr = date('d/m',strtotime($event->start->dateTime));
-
-	$output['events'][$i]['name'] = $eventName;
-	$output['events'][$i]['date'] = $eventDateStr;
+	$eventMonth = date('n',strtotime($event->start->dateTime));
+	if ($eventMonth == $currentMonth){
+		$monthStr = "Deze Maand";
+	}else{
+		$monthStr = date('F',strtotime($event->start->dateTime));
+	}
+	$output['events'][$monthStr][$i]['name'] = $eventName;
+	$output['events'][$monthStr][$i]['date'] = $eventDateStr;	
 	$i++;
 }
 
