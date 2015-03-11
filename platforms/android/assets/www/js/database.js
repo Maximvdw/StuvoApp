3ade8493a1eaf2d20a5973355981bd3f8c02defb
Maxim Van de Wynckel <maxim@mvdw-software.com>
2015-03-11 11:34:53 -0400

    calendar add + database

--- platforms/android/assets/www/js/database.js
+++ platforms/android/assets/www/js/database.js
@@ -8,8 +8,8 @@ function onDeviceReady() {
     tx.executeSql('DROP TABLE IF EXISTS settings');
     tx.executeSql('CREATE TABLE IF NOT EXISTS settings (setting varchar(50) primary key, value varchar(50)');
 
-    tx.executeSql("INSERT INTO settings (setting,value) VALUES (?,?)", ["notifications_events", "0"], function(tx, res){}, function(e){});
-    tx.executeSql("INSERT INTO settings (setting,value) VALUES (?,?)", ["notifications_news", "1"], function(tx, res){}, function(e){});
+    tx.executeSql("INSERT INTO settings (setting,value) VALUES (?,?)", ["notifications_events", "0"], function(tx, res){ alert(res.rows.item(0)); }, function(e){});
+    tx.executeSql("INSERT INTO settings (setting,value) VALUES (?,?)", ["notifications_news", "1"], function(tx, res) { alert(res.rows.item(0)); }, function(e){});
     
       db.transaction(function(tx) {
         tx.executeSql("select value from settings where setting = 'notifications_events';", [], function(tx, res) {
