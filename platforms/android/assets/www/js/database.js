094745519bc9ed5f52b261c7b879ac3e9af1aab0
Maxim Van de Wynckel <maxim@mvdw-software.com>
2015-03-11 12:40:11 -0400

    removed debug messages

--- platforms/android/assets/www/js/database.js
+++ platforms/android/assets/www/js/database.js
@@ -7,13 +7,22 @@ function onDeviceReady() {
   db.transaction(function(tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS settings (setting varchar(50) primary key, value varchar(50)');
 
-    // demonstrate PRAGMA:
-    db.executeSql("pragma table_info (settings);", [], function(res) {
-      console.log("PRAGMA res: " + JSON.stringify(res));
-      alert(JSON.stringify(res))
-    });
-
     tx.executeSql("INSERT INTO settings (setting,value) VALUES (?,?)", ["notifications_events", "0"], function(tx, res) {
+      console.log("insertId: " + res.insertId + " -- probably 1");
+      alert("rowsAffected: " + res.rowsAffected + " -- should be 1");
+
+      db.transaction(function(tx) {
+        tx.executeSql("select count(setting) as cnt from settings;", [], function(tx, res) {
+          console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+          console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+        });
+      });
+
+    }, function(e) {
+      console.log("ERROR: " + e.message);
+      alert(e.message);
+    });
+     tx.executeSql("INSERT INTO settings (setting,value) VALUES (?,?)", ["notifications_news", "0"], function(tx, res) {
       console.log("insertId: " + res.insertId + " -- probably 1");
       alert("rowsAffected: " + res.rowsAffected + " -- should be 1");
 
--- www/js/calendar.js
+++ www/js/calendar.js
@@ -1,10 +1,8 @@
 function addToCalendar(title,location,notes,startyear,startmonth,startday,starthour,startminute,endyear,endmonth,endday,endhour,endminute){
-   alert("test");
 	// prep some variables
-  var startDate = new Date(startyear,startmonth-1,startday,starthour,startminute,0,0,0); // beware: month 0 = january, 11 = december
-  var endDate = new Date(endyear,endmonth-1,endday,endhour,endminute,0,0,0);
-alert(startDate);
-var success = function(message) { alert("Success: " + JSON.stringify(message)); };
+  var startDate = new Date(startyear,startmonth-1,startday,starthour,startminute,0,0); // beware: month 0 = january, 11 = december
+  var endDate = new Date(endyear,endmonth-1,endday,endhour,endminute,0,0);
+  var success = function(message) {};
   var error = function(message) { alert("Error: " + message); };
 
   // create an event interactively
