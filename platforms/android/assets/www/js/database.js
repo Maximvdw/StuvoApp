f639a8fe30eddd1a3a5fa653a1745d190bee7eae
Maxim Van de Wynckel <maxim@mvdw-software.com>
2015-03-12 11:08:21 -0400

    notification test

--- platforms/android/assets/www/js/database.js
+++ platforms/android/assets/www/js/database.js
@@ -3,46 +3,47 @@ document.addEventListener("deviceready", onDeviceReady, false);
 var db = null;
 // Cordova is ready
 function onDeviceReady() {
-  db = window.sqlitePlugin.openDatabase({name: "stuvo.db"});
-  resetSettings();
-  db.transaction(function(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS settings (setting varchar(50) primary key, settingvalue varchar(50)');
-
-    tx.executeSql("INSERT INTO settings (setting,settingvalue) VALUES (?,?)", ["notifications_events", "0"], function(tx, res) {
-
-    }, function(e) {
-
-    });
-      db.transaction(function(tx) {
-        tx.executeSql("select count(setting) as cnt from settings;", [], function(tx, res) {
-          console.log("res.rows.length: " + res.rows.length + " -- should be 1");
-          console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
-          alert( res.rows.item(0).cnt);
-        });
-      });
-      
-      
-     tx.executeSql("INSERT INTO settings (setting,settingvalue) VALUES (?,?)", ["notifications_news", "0"], function(tx, res) {
-
-    }, function(e) {
-    });
-    
-          db.transaction(function(tx) {
-        tx.executeSql("select count(setting) as cnt from settings;", [], function(tx, res) {
-          console.log("res.rows.length: " + res.rows.length + " -- should be 1");
-          console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
-             alert( res.rows.item(0).cnt);
-        });
-      });
-  });
-}
-
-function setSetting(setting,value){
-	
-}
-
-function resetSettings(){
+	db = window.sqlitePlugin.openDatabase({
+		name: "stuvo.db"
+	});
+	resetSettings();
 	db.transaction(function(tx) {
-	 	tx.executeSql('DROP TABLE IF EXISTS settings');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS settings (setting varchar(50) primary key, settingvalue varchar(50)');
+
+		tx.executeSql("INSERT INTO settings (setting,settingvalue) VALUES (?,?)", ["notifications_events", "0"], function(tx, res) {
+
+		}, function(e) {
+
+		});
+		db.transaction(function(tx) {
+			tx.executeSql("select count(setting) as cnt from settings;", [], function(tx, res) {
+				console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+				console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+				alert(res.rows.item(0).cnt);
+			});
+		});
+
+
+		tx.executeSql("INSERT INTO settings (setting,settingvalue) VALUES (?,?)", ["notifications_news", "0"], function(tx, res) {
+
+		}, function(e) {});
+
+		db.transaction(function(tx) {
+			tx.executeSql("select count(setting) as cnt from settings;", [], function(tx, res) {
+				console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+				console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+				alert(res.rows.item(0).cnt);
+			});
+		});
+	});
+}
+
+function setSetting(setting, value) {
+
+}
+
+function resetSettings() {
+	db.transaction(function(tx) {
+		tx.executeSql('DROP TABLE IF EXISTS settings');
 	});
 }
--- www/js/notifications.js
+++ www/js/notifications.js
@@ -1,12 +1,6 @@
 document.addEventListener("deviceready", function() {
 	// Schedule notification for tomorrow to remember about the meeting
-	cordova.plugins.notification.local.schedule({
-		id: 10,
-		title: "Meeting in 15 minutes!",
-		text: "Jour fixe Produktionsbesprechung",
-		at: today_at_3_55_pm,
-		data: {
-			meetingId: "#123FG8"
-		}
+	window.plugin.notification.local.add({
+		message: 'Great app!'
 	});
 });
