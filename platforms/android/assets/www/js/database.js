8e45a2332364be6486db2b34ca75eb458e55862e
Maxim Van de Wynckel <maxim@mvdw-software.com>
2015-03-15 08:50:56 -0400

    database debugging

--- platforms/android/assets/www/js/database.js
+++ platforms/android/assets/www/js/database.js
@@ -8,20 +8,20 @@ function onDeviceReady() {
 	});
 
 	db.transaction(function(tx) {
-		tx.executeSql('DROP TABLE IF EXISTS test_table');
-		tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
+		tx.executeSql('DROP TABLE IF EXISTS settings');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS settings (key text primary key, value text)');
 
 		// demonstrate PRAGMA:
-		db.executeSql("pragma table_info (test_table);", [], function(res) {
+		db.executeSql("pragma table_info (settings);", [], function(res) {
 			console.log("PRAGMA res: " + JSON.stringify(res));
 		});
 
-		tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
+		tx.executeSql("INSERT INTO settings (key, value) VALUES (?,?)", ["test", "100"], function(tx, res) {
 			console.log("insertId: " + res.insertId + " -- probably 1");
 			console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
 
 			db.transaction(function(tx) {
-				tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
+				tx.executeSql("select count(key) as cnt from settings;", [], function(tx, res) {
 					console.log("res.rows.length: " + res.rows.length + " -- should be 1");
 					console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
 				});
