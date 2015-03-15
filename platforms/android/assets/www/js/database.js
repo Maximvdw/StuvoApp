38b4471c61dcd3f3b34dae7fb6a45b4dff3c6ee9
Maxim Van de Wynckel <maxim@mvdw-software.com>
2015-03-15 08:56:15 -0400

    database changes

--- platforms/android/assets/www/js/database.js
+++ platforms/android/assets/www/js/database.js
@@ -8,7 +8,6 @@ function onDeviceReady() {
 	});
 
 	db.transaction(function(tx) {
-		tx.executeSql('DROP TABLE IF EXISTS settings');
 		tx.executeSql('CREATE TABLE IF NOT EXISTS settings (key text primary key, value text)');
 
 		// demonstrate PRAGMA:
@@ -19,16 +18,15 @@ function onDeviceReady() {
 		tx.executeSql("INSERT INTO settings (key, value) VALUES (?,?)", ["test", "100"], function(tx, res) {
 			console.log("insertId: " + res.insertId + " -- probably 1");
 			console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
-
-			db.transaction(function(tx) {
-				tx.executeSql("select count(key) as cnt from settings;", [], function(tx, res) {
-					console.log("res.rows.length: " + res.rows.length + " -- should be 1");
-					console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
-				});
-			});
-
 		}, function(e) {
 			console.log("ERROR: " + e.message);
+		});
+
+		db.transaction(function(tx) {
+			tx.executeSql("select count(key) as cnt from settings;", [], function(tx, res) {
+				console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+				console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+			});
 		});
 	});
 }
