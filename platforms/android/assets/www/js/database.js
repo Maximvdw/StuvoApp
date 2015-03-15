40f47286077ca185905534c341bd6865b9a132f5
Maxim Van de Wynckel <maxim@mvdw-software.com>
2015-03-15 09:06:38 -0400

    database get value

--- platforms/android/assets/www/js/database.js
+++ platforms/android/assets/www/js/database.js
@@ -15,7 +15,13 @@ function onDeviceReady() {
 			console.log("PRAGMA res: " + JSON.stringify(res));
 		});
 
-		tx.executeSql("INSERT INTO settings (key, value) VALUES (?,?)", ["test", "100"], function(tx, res) {
+		tx.executeSql("INSERT INTO settings (key, value) VALUES (?,?)", ["notification_news", "YEs"], function(tx, res) {
+			console.log("insertId: " + res.insertId + " -- probably 1");
+			console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+		}, function(e) {
+			console.log("ERROR: " + e.message);
+		});
+		tx.executeSql("INSERT INTO settings (key, value) VALUES (?,?)", ["notification_events", "No"], function(tx, res) {
 			console.log("insertId: " + res.insertId + " -- probably 1");
 			console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
 		}, function(e) {
@@ -23,9 +29,9 @@ function onDeviceReady() {
 		});
 
 		db.transaction(function(tx) {
-			tx.executeSql("select count(key) as cnt from settings;", [], function(tx, res) {
+			tx.executeSql("select value from settings where key = 'notification_news';", [], function(tx, res) {
 				console.log("res.rows.length: " + res.rows.length + " -- should be 1");
-				console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+				console.log("res.rows.item(0).value: " + res.rows.item(0).value);
 			});
 		});
 	});
