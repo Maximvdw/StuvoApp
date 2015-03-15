3627cdbae0b53ff4872d5baf07c4aa0517c2dea7
Maxim Van de Wynckel <maxim@mvdw-software.com>
2015-03-15 09:22:07 -0400

    database changes and setting

--- www/js/database.js
+++ www/js/database.js
@@ -1,6 +1,12 @@
 // Wait for Cordova to load
 document.addEventListener("deviceready", onDeviceReady, false);
 var db = null;
+
+var settings = {
+	notifications_news: false,
+	notifications_events: false
+};
+
 // Cordova is ready
 function onDeviceReady() {
 	var db = window.sqlitePlugin.openDatabase({
@@ -15,30 +21,49 @@ function onDeviceReady() {
 			console.log("PRAGMA res: " + JSON.stringify(res));
 		});
 
-		tx.executeSql("INSERT INTO settings (key, value) VALUES (?,?)", ["notification_news", "YEs"], function(tx, res) {
+		tx.executeSql("INSERT INTO settings (key, value) VALUES (?,?)", ["notification_news", "0"], function(tx, res) {
 			console.log("insertId: " + res.insertId + " -- probably 1");
 			console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
 		}, function(e) {
 			console.log("ERROR: " + e.message);
 		});
-		tx.executeSql("INSERT INTO settings (key, value) VALUES (?,?)", ["notification_events", "No"], function(tx, res) {
+		tx.executeSql("INSERT INTO settings (key, value) VALUES (?,?)", ["notification_events", "0"], function(tx, res) {
 			console.log("insertId: " + res.insertId + " -- probably 1");
 			console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
 		}, function(e) {
 			console.log("ERROR: " + e.message);
 		});
 
-		db.transaction(function(tx) {
-			tx.executeSql("select value from settings where key = 'notification_news';", [], function(tx, res) {
-				console.log("res.rows.length: " + res.rows.length + " -- should be 1");
-				console.log("res.rows.item(0).value: " + res.rows.item(0).value);
-			});
+		tx.executeSql("select value from settings where key = 'notification_news';", [], function(tx, res) {
+			console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+			console.log("res.rows.item(0).value: " + res.rows.item(0).value);
+			if (res.rows.item(0).value == "1")
+				settings.notifications_news = true;
+			else
+				settings.notifications_news = false;
+		});
+		tx.executeSql("select value from settings where key = 'notification_events';", [], function(tx, res) {
+			console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+			console.log("res.rows.item(0).value: " + res.rows.item(0).value);
+			if (res.rows.item(0).value == "1")
+				settings.notifications_events = true;
+			else
+				settings.notifications_events = false;
 		});
 	});
+
+	setSetting("notification_news", "1");
 }
 
 function setSetting(setting, value) {
-
+	db.transaction(function(tx) {
+		tx.executeSql("UPDATE TABLE settings SET value = ? WHERE key = ?", [value, setting], function(tx, res) {
+			console.log("insertId: " + res.insertId + " -- probably 1");
+			console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+		}, function(e) {
+			console.log("ERROR: " + e.message);
+		});
+	});
 }
 
 function resetSettings() {
